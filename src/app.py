5fae365838b4f13c2ad407641e9fd0f2cac40253
Trevor-Yamada <thyama22@colby.edu>
2021-11-09 17:11:47 -0500

    Created database.py moved app.py, changed case.html

--- src/app.py
+++ src/app.py
@@ -1,12 +1,34 @@
 
 
-from flask import Flask,render_template
-        
-app = Flask(__name__)
+from flask import Flask,render_template, request, url_for, redirect
 
+import database
+
+
+app = Flask(__name__)
+visitors = []
 @app.route("/")
 def index():
     user = {'username' : 'Trevor'}
-    return render_template("base.html",title="home",user=user)
+    return render_template("base.html",title="home",user=user,visitors = visitors)
+
+
+@app.route("/about")
+def about():
+	return "About us: Naser and the cool kids from CS321"
+
+@app.route("/add", methods=["POST"])
+def add():
+	visitor = request.form.get("visitor")
+	visitors.append(visitor)
+	print(visitors[-1])
+
+	return redirect(url_for("index"))
+
+@app.route("/remove/<string:name>")
+def remove(name):
+	visitors.remove(name)
+	return redirect(url_for("index"))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ src/database.py
@@ -0,0 +1,6 @@
+
+
+
+def query_name():
+
+	return 
